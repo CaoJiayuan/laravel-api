fc1da23bc3f2d8bbb008b4bd5fb5acc73f63e417
Cao Jiayuan <cjy632258@hotmail.com>
2017-11-24 16:49:51 +0800

    update

--- src/CaoJiayuan/LaravelApi/Auth/SimpleJWTAuthController.php
+++ src/CaoJiayuan/LaravelApi/Auth/SimpleJWTAuthController.php
@@ -10,33 +10,75 @@ namespace CaoJiayuan\LaravelApi\Auth;
 
 
 use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class SimpleJWTAuthController extends Controller
 {
 
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
     /**
      * The user has been authenticated.
      *
-     * @param  Request  $request
-     * @param  mixed  $user
+     * @param  Request $request
+     * @param  mixed $user
      * @return mixed
      */
     protected function authenticated(Request $request, $user)
     {
-        $user->token = $this->getJWTToken($request, $user);
+        $data = [
+            'access_token' => $this->getJWTToken($request, $user),
+            'expires_in'   => $this->getTTl(),
+            'type'         => 'bearer',
+            'user'         => $user
+        ];
 
-        return $user;
+        return response()->json($data);
     }
 
     protected function getJWTToken(Request $request, $user)
     {
-        return JWTAuth::fromUser($user, $this->getCustomClaims($request, $user));
+        /** @var JWTGuard $guard */
+        $guard = \Auth::guard('jwt');
+        return $guard->login($user);
     }
 
-    protected function getCustomClaims(Request $request, $user)
+    public function getTTl()
     {
-        return [];
+        $guard = \Auth::guard('jwt');
+
+        return $guard->factory()->getTTL() * 60;
+    }
+
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+        $request->session()->invalidate();
+        $jwtGuard = \Auth::guard('jwt');
+        try {
+            $jwtGuard->logout();
+        } catch (\Exception $exception) {
+        }
+
+        return response()->json(['message' => 'Success']);
     }
 }
