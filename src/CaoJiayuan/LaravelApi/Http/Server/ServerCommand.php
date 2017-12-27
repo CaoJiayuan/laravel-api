8564a9fdbba296ad279260802a05265641eb1938
cjy <cjy632258@hotmail.com>
2017-12-27 16:39:27 +0800

    update

--- src/CaoJiayuan/LaravelApi/Http/Server/ServerCommand.php
+++ src/CaoJiayuan/LaravelApi/Http/Server/ServerCommand.php
@@ -118,7 +118,7 @@ class ServerCommand extends Command
             $this->kernel->terminate($request, $response);
             $requestEnd = microtime(true);
             $requestTime = round($requestEnd - $requestStart, 4);
-            $d || $this->info("Handle request ({$request->getMethod()})[{$request->url()}] from [{$request->ip()}], status: ({$response->status()}), time : [$requestTime]");
+            $d || $this->info("Handle request ({$request->getMethod()})[{$request->url()}] from [{$request->ip()}], status: ({$response->status()}), time : [{$requestTime}s]");
         };
 
         Worker::runAll();
