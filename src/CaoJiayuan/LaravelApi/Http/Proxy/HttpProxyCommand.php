1c1415bb999b70e3e5e57bcb27154060090f9cea
cjy <cjy632258@hotmail.com>
2018-03-26 17:20:46 +0800

    name

--- src/CaoJiayuan/LaravelApi/Http/Proxy/HttpProxyCommand.php
+++ src/CaoJiayuan/LaravelApi/Http/Proxy/HttpProxyCommand.php
@@ -10,9 +10,9 @@ namespace CaoJiayuan\LaravelApi\Http\Proxy;
 
 
 use Illuminate\Console\Command;
+use Workerman\Connection\AsyncTcpConnection;
 use Workerman\Connection\TcpConnection;
 use Workerman\Worker;
-use Workerman\Connection\AsyncTcpConnection;
 
 class HttpProxyCommand extends Command
 {
@@ -22,7 +22,7 @@ class HttpProxyCommand extends Command
      * @var string
      */
 
-    protected $signature = 'laravel-api:proxy {cmd=start : Command to send} {--port=8080 : Listen port} {--count=4 : Work process} {--daemon=1 : Daemon mode}';
+    protected $signature = 'api-util:proxy {cmd=start : Command to send} {--port=8080 : Listen port} {--count=4 : Work process} {--daemon=1 : Daemon mode}';
 
     /**
      * The console command description.
@@ -30,6 +30,10 @@ class HttpProxyCommand extends Command
      * @var string
      */
     protected $description = 'A http proxy server using workerman';
+
+    protected $options = [
+
+    ];
 
     public function handle()
     {
@@ -48,17 +52,18 @@ class HttpProxyCommand extends Command
         $worker = new Worker("tcp://0.0.0.0:{$port}");
         $worker->count = $count;
         $worker->name = 'http-proxy';
+
         /**
          * @param TcpConnection $connection
          * @param $buffer
          */
-        $worker->onMessage = function($connection, $buffer) use ($d)
-        {
+        $worker->onMessage = function ($connection, $buffer) use ($d) {
             list($method, $address, $HttpVersion) = explode(' ', $buffer);
             $UrlData = parse_url($address);
             $address = !isset($UrlData['port']) ? "{$UrlData['host']}:80" : "{$UrlData['host']}:{$UrlData['port']}";
 
-            $remoteConnection = new AsyncTcpConnection("tcp://$address");
+
+            $remoteConnection = new AsyncTcpConnection("tcp://$address", $this->options);
             if ($method !== 'CONNECT') {
                 $remoteConnection->send($buffer);
             } else {
@@ -66,7 +71,7 @@ class HttpProxyCommand extends Command
             }
             $d || $this->info("[$method] $address");
 
-            $remoteConnection ->pipe($connection);
+            $remoteConnection->pipe($connection);
             $connection->pipe($remoteConnection);
             $remoteConnection->connect();
         };
