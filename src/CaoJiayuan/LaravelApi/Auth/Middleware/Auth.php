12278e7c3b383718b60e8a00eb13aaa1df9c08ab
cjy <cjy632258@hotmail.com>
2018-04-24 18:13:52 +0800

    update

--- src/CaoJiayuan/LaravelApi/Auth/Middleware/Auth.php
+++ src/CaoJiayuan/LaravelApi/Auth/Middleware/Auth.php
@@ -1,37 +1,70 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: caojiayuan
- * Date: 17-9-8
- * Time: 下午2:38
+ * Not necessary login, parse user from JWT
  */
 
 namespace CaoJiayuan\LaravelApi\Auth\Middleware;
 
 
+use App\User;
 use Closure;
-use Illuminate\Auth\Middleware\Authenticate;
-use Illuminate\Foundation\Auth\User;
+use Illuminate\Auth\AuthManager;
 use Illuminate\Http\Request;
-use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\JWT;
+use Tymon\JWTAuth\JWTGuard;
 use Tymon\JWTAuth\Payload;
 
-class Auth extends Authenticate
+class Auth
 {
+    protected $guard = null;
 
-
-
-    public function handle($request, Closure $next, ...$guards)
+    /**
+     * @param null $name
+     * @return \Illuminate\Contracts\Auth\Guard|\Illuminate\Contracts\Auth\StatefulGuard|JWTGuard|JWT
+     */
+    protected function guard($name = null)
     {
-        $token = $this->getToken($request);
-        $payload = JWTAuth::getPayload($token);
+        /** @var AuthManager $auth */
+        $auth = app('auth');
+        return $auth->guard($name);
+    }
 
-        $this->authenticateWithPayload($payload);
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Request $request
+     * @param  \Closure $next
+     * @param  string|null $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        $this->guard = $guard;
+        try {
+            $token = $this->getToken($request);
+            $payload =  $this->guard($guard)->getPayload($token);
+
+            $user = $this->authenticateWithPayload($payload);
+        } catch (\Exception $e) {
+
+        }
+
 
         return $next($request);
     }
 
+    /**
+     * @param Request $request
+     * @return bool|string
+     */
+    protected function getToken($request)
+    {
+        if (!$token = $this->guard($this->guard)->getToken()) {
+            $token = $request->bearerToken();
+        }
 
+        return $token;
+    }
 
     /**
      * @param Payload $payload
@@ -54,20 +87,12 @@ class Auth extends Authenticate
      */
     public function getUser($id, $payload)
     {
-        return $this->auth->loginUsingId($id);
-    }
-
-    /**
-     * @param Request $request
-     * @return bool|string
-     */
-    protected function getToken($request)
-    {
-        if (!$token = JWTAuth::getToken()) {
-            $token = $request->bearerToken();
+        if (empty($id)) {
+            return null;
         }
 
-        return $token;
+        $jwt = $this->guard($this->guard);
+        $jwt->byId($id);
+        return $jwt->user();
     }
-
 }
