6e1ba39d29e18051c74e3f721ac06dd8fb02f921
cjy <cjy632258@hotmail.com>
2018-05-08 15:32:32 +0800

    request

--- src/CaoJiayuan/LaravelApi/Http/Request/RequestHelper.php
+++ src/CaoJiayuan/LaravelApi/Http/Request/RequestHelper.php
@@ -7,67 +7,67 @@ use Illuminate\Http\Request;
 trait RequestHelper
 {
 
-  public function getValidatedData(array $rules, array $messages = [], array $customAttributes = [])
-  {
-    list($keys, $fixedRules) = $this->resolveRules($rules);
+    public function getValidatedData(array $rules, array $messages = [], array $customAttributes = [])
+    {
+        /** @var Request $request */
+        $request = app('request');
+        list($keys, $fixedRules) = $this->resolveRules($rules, $request);
 
-    /** @var Request $request */
-    $request = app('request');
-    $this->validate($request, $fixedRules, $messages, $customAttributes);
+        $this->validate($request, $fixedRules, $messages, $customAttributes);
 
-    $data = [];
-    foreach ($keys  as $key => $resolver) {
-      $data[$key] = $resolver($request->get($key));
-    }
-    return $data;
-  }
-
-  protected function resolveRules(array $rules)
-  {
-    $keys = [];
-    $fixedRules = [];
-    foreach ($rules as $key => $rule) {
-      if (!is_numeric($key)) { // has role
-        list($r, $resolver) = $this->getRulesFromValue($rule);
-        $r && $fixedRules[$key] = $r;
-        str_contains($key, '.') || $keys[$key] = $resolver;
-      } else {
-        $keys[$rule] = function ($v) {
-          return $v;
-        };
-      }
-    }
-
-    return [$keys, $fixedRules];
-  }
-
-  protected function getRulesFromValue($value)
-  {
-    $resolver = function ($v) {
-      return $v;
-    };
-
-    if (is_array($value)) {
-
-      if (isset($value[0])) { // ex: ['required']
-        $rule = $value[0];
-      } else {
-        $rule = null;
-      }
-
-      if (isset($value[1])) {
-        if (is_callable($value[1])) { // ex: ['required', function($v) { return $v + 1;}]
-          $resolver = $value[1];
-        } else { // ex: ['required', 1]
-          $resolver = function () use ($value) {
-            return $value[1];
-          };
+        $data = [];
+        foreach ($keys as $key => $resolver) {
+            $data[$key] = $resolver($request->get($key));
         }
-      }
-    } else {
-      $rule = $value;
+        return $data;
     }
 
-    return [$rule, $resolver];
-  }
+    protected function resolveRules(array $rules, $request)
+    {
+        $keys = [];
+        $fixedRules = [];
+        foreach ($rules as $key => $rule) {
+            if (!is_numeric($key)) { // has role
+                list($r, $resolver) = $this->getRulesFromValue($rule, $request);
+                $r && $fixedRules[$key] = $r;
+                str_contains($key, '.') || $keys[$key] = $resolver;
+            } else {
+                $keys[$rule] = function ($v) {
+                    return $v;
+                };
+            }
+        }
+
+        return [$keys, $fixedRules];
+    }
+
+    protected function getRulesFromValue($value, $request)
+    {
+        $resolver = function ($v) {
+            return $v;
+        };
+
+        if (is_array($value)) {
+
+            if (isset($value[0])) { // ex: ['required']
+                $rule = $value[0];
+            } else {
+                $rule = null;
+            }
+
+            if (isset($value[1])) {
+                if (is_callable($value[1])) { // ex: ['required', function($v) { return $v + 1;}]
+                    $resolver = $value[1];
+                } else { // ex: ['required', 1]
+                    $resolver = function ($v) use ($value) {
+                        return $v === null ? $value[1] : $v;
+                    };
+                }
+            }
+        } else {
+            $rule = $value;
+        }
+
+        return [is_callable($rule) ? call_user_func($rule, $request) : $rule, $resolver];
+    }
 }
