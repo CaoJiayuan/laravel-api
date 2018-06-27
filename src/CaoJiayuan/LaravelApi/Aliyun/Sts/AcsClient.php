5f72ad162e84186afe57e9629167276242a6fb3c
cjy <cjy632258@hotmail.com>
2018-06-27 17:36:47 +0800

    sts

---
+++ src/CaoJiayuan/LaravelApi/Aliyun/Sts/AcsClient.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cjy
+ * Date: 2018/6/27
+ * Time: 下午5:23
+ */
+
+namespace CaoJiayuan\LaravelApi\Aliyun\Sts;
+
+
+use Sts\Core\DefaultAcsClient;
+
+class AcsClient extends DefaultAcsClient
+{
+
+}
--- src/CaoJiayuan/LaravelApi/Aliyun/Sts/StsAuth.php
+++ src/CaoJiayuan/LaravelApi/Aliyun/Sts/StsAuth.php
@@ -11,12 +11,14 @@ namespace CaoJiayuan\LaravelApi\Aliyun\Sts;
 
 use Sts\AssumeRoleRequest;
 use Sts\Core\DefaultAcsClient;
+use Sts\Core\Http\HttpHelper;
 use Sts\Core\Profile\DefaultProfile;
+use Sts\Core\Regions\EndpointProvider;
 
 class StsAuth
 {
 
-    public static function auth($roleName, $regionId = "cn-hangzhou")
+    public static function auth($roleName, $regionId = "cn-hangzhou", $maxRetryNumber = 3)
     {
         $re = new AssumeRoleRequest();
         $accessKeyID = config('aliyun_sts.key');
@@ -30,7 +32,15 @@ class StsAuth
         $re->setRoleArn($roleArn);
         $re->setPolicy(json_encode($policy));
         $re->setDurationSeconds($tokenExpire);
-        $response = $cli->doAction($re);
+        $response = $cli->doAction($re, null, null, false);
+        $retryTimes = 0;
+        $domain = EndpointProvider::findProductDomain($re->getRegionId(), $re->getProduct());
+
+        while (500 <= $response->getStatus() && $retryTimes < $maxRetryNumber) {
+            $requestUrl = $re->composeUrl(null, null, $domain);
+            $response = HttpHelper::curl($requestUrl, null, $re->getHeaders());
+            $retryTimes ++;
+        }
 
         $result = json_decode($response->getBody(), true);
 
