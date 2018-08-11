3e18efc8aee66af9d6ea386119b80008db177afe
caojiayuan <cjy632258@hotmail.com>
2018-08-11 15:31:11 +0800

    data transfer

--- src/CaoJiayuan/LaravelApi/Data/Transfer.php
+++ src/CaoJiayuan/LaravelApi/Data/Transfer.php
@@ -23,17 +23,11 @@ class Transfer implements ArrayAccess, Arrayable
         'time' => 'timestamp'
     ];
 
-    public function __construct($data)
-    {
-        $this->data = $this->morphData($data);
-    }
-
     protected function morphData($data)
     {
         if ($data instanceof Arrayable) {
             return $data->toArray();
         }
-
 
         return $data;
     }
@@ -66,7 +60,7 @@ class Transfer implements ArrayAccess, Arrayable
         }
 
         return array_map(function ($item) use ($template) {
-            return (new self($item))->transform($template);
+            return (new static())->setData($item)->transform($template);
         }, array_wrap($this->data));
     }
 
@@ -84,13 +78,20 @@ class Transfer implements ArrayAccess, Arrayable
         return Carbon::parse($v)->timestamp;
     }
 
+    public function formatTrim($v)
+    {
+        return trim($v);
+    }
+
     public function take($key)
     {
         if (is_null($key)) {
             return $this;
         }
 
-        return new self(data_get($this->data, $key));
+        $transfer = new static();
+
+        return $transfer->setData(data_get($this->data, $key));
     }
 
     protected function transformWithTemplate($template)
@@ -227,5 +228,15 @@ class Transfer implements ArrayAccess, Arrayable
     public function toArray()
     {
         return $this->data;
+    }
+
+    /**
+     * @param array $data
+     * @return static
+     */
+    public function setData($data)
+    {
+        $this->data = $this->morphData($data);
+        return $this;
     }
 }
